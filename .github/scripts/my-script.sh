dd7dac6212d4a361f74d99db09db65e112fa6b1d
mkacem <moez.kacem@keyrus.com>
2024-10-30 15:56:53 +0100

    Update my-script.sh

--- .github/scripts/my-script.sh
+++ .github/scripts/my-script.sh
@@ -1,12 +1,12 @@
 #/bin/sh
 
 file="../../app.properties" 
-diff=$(git diff --unified=0 HEAD~2 -- $file  | grep -Ev '(index|@@|--git|---|\+\+\+|new)' | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' )
-fileM=$(git diff --name-only HEAD~2 -- $file)
+diff=$(git diff --unified=0 origin/HEAD~1 -- $file  | grep -Ev '(index|@@|--git|---|\+\+\+|new)' | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' )
+fileM=$(git diff --name-only origin/HEAD~1 -- $file)
 date=$(date '+%Y-%m-%d %H:%M:%S')
 sep1="\n____________________________________________\n"
 sep2='\n--------------------------------------------\n'
-echo $fileM
+echo $diff
 if [ -f "$file" ] && [ "$diff" ]
 then  
   echo -e $sep1$date'   '$fileM$sep2$diff$sep1 >> changelog.txt
