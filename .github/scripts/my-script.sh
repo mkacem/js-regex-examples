7adbd79da25119ac9ca3cbdf9c29a6fba913a36c
mkacem <moez.kacem@keyrus.com>
2024-10-30 12:15:50 +0100

    Update my-script.sh

--- .github/scripts/my-script.sh
+++ .github/scripts/my-script.sh
@@ -8,12 +8,13 @@ then
 
   while IFS='=' read -r key value
   do
-    key=$(echo $key | tr '.' '_')
-    eval ${key}=\${value}
+    echo "key=${key} value=${value}"
+    # key=$(echo $key | tr '.' '_')
+    # eval ${key}=\${value}
   done < "$file"
 
-  echo "User Id       = " ${db_uat_user}
-  echo "user password = " ${db_uat_passwd}
+  # echo "User Id       = " ${db_uat_user}
+  # echo "user password = " ${db_uat_passwd}
 else
-  echo "$file not found."
+  echo "**** $file not found."
 fi
