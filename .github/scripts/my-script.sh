7136140c2c513e2a6903252f5ab22cbaf2db0d50
mkacem <moez.kacem@keyrus.com>
2024-10-30 12:09:50 +0100

    test

--- .github/scripts/my-script.sh
+++ .github/scripts/my-script.sh
@@ -1,2 +1,19 @@
-#! /bin/bash
-echo "this is .sh script.."
+#/bin/sh
+
+file="../../app.properties"
+
+if [ -f "$file" ]
+then
+  echo "$file found."
+
+  while IFS='=' read -r key value
+  do
+    key=$(echo $key | tr '.' '_')
+    eval ${key}=\${value}
+  done < "$file"
+
+  echo "User Id       = " ${db_uat_user}
+  echo "user password = " ${db_uat_passwd}
+else
+  echo "$file not found."
+fi
