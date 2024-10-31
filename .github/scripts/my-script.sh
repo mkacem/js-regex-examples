edb599dd00f565859d5822e497d157d781c80077
mkacem <moez.kacem@keyrus.com>
2024-10-31 10:53:38 +0100

    test checkout

--- .github/scripts/my-script.sh
+++ .github/scripts/my-script.sh
@@ -2,18 +2,18 @@
 propfiles=$(find ../../ -type f -name '*\.properties')
 for file in $propfiles; do
   echo "Processing $file"
-  diff=$(git diff --unified=0 HEAD origin/dev -- $file  | grep -Ev '(index|@@|--git|---|\+\+\+|new)' | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' )
-  fileM=$(git diff --name-status HEAD origin/dev -- $file)
+  diff=$(git diff --unified=0  origin/dev HEAD -- $file  | grep -Ev '(index|@@|--git|---|\+\+\+|new)' | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' )
+  fileM=$(git diff --name-status  origin/dev HEAD -- $file)
   date=$(date '+%Y-%m-%d %H:%M:%S')
   sep1="____________________________________________\n"
   sep2='\n--------------------------------------------\n'
   echo -e '\n***********>>>>>'$file
   
-  echo $(git diff --name-status HEAD origin/dev -- $file)
+  echo $(git diff --name-status origin/dev HEAD -- $file)
   echo -e '\n***********>>>>>'
   if [ -f "$file" ] && [ "$diff" ]
   then  
-    echo -e $sep1'\n'$date'   '$fileM$sep2$diff'\n'$sep1 >> changelog.txt
+    echo -e $sep1$date'   '$fileM$sep2$diff'\n'$sep1 >> changelog.txt
     grep -v '^#' < "$file" | 
       while IFS='=' read -r key value
       do
