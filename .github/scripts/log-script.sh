5cc5a6c4d901c3f233c086e4573bd1967be31c7b
mkacem <moez.kacem@keyrus.com>
2024-10-31 16:48:53 +0100

    Update log-script.sh

--- .github/scripts/log-script.sh
+++ .github/scripts/log-script.sh
@@ -1,24 +1,33 @@
 #/bin/sh
-propfiles=$(find ../../ -type f -name '*\.properties')
+propfiles=$(find ../../ -type f -name '*\.properties' )
+commitDate=false
+i=1
+count=$(find ../../ -type f -name '*\.properties'  | wc -l)
 for file in $propfiles; do
-  echo "Processing $file"
-  diff=$(git diff --unified=0 --ignore-all-space --ignore-cr-at-eol --ignore-space-at-eol --ignore-blank-lines HEAD^^ HEAD -- $file  | grep -Ev '(index|@@|--git|---|\+\+\+|new)' | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' )
+  
+  echo "Processing $file $i"  
+  diff=$(git diff --unified=0 --ignore-all-space --ignore-cr-at-eol --ignore-space-at-eol --ignore-blank-lines HEAD^^ HEAD -- $file  | grep -Ev '(index|@@|--git|---|\+\+\+|new)' | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' | sed -e 's/\+\\n//g' -e 's/-\\n//g' -e 's/\\n+$//g'  -e 's/\\n-$//g'  )
   fileM=$(git diff --name-status --ignore-all-space --ignore-blank-lines HEAD^^ HEAD -- $file)
+  echo $diff
   date=$(date '+%Y-%m-%d %H:%M:%S')
-  sep1="____________________________________________\n"
+  sep1="============================================="
   sep2='\n--------------------------------------------\n'
+  sep3='\n============================================\n'
   if [ -f "$file" ] && [ "$diff" ]
-  then  
-    echo -e $sep1$date'   '$fileM$sep2$diff'\n'$sep1 >> ../../env.log.txt
-    # grep -v '^#' < "$file" | 
-    #   while IFS='=' read -r key value
-    #   do
-    #     if [ "$key" ] 
-    #     then
-    #       testKey="${key}"
-    #     fi
-    #   done 
+  then 
+    if [ "$commitDate" = false ] 
+    then
+      output=$date$sep3$fileM$sep2$diff
+    else
+      output=$sep2$fileM$sep2$diff
+    fi    
+    echo -e $output >> ../../env.log.txt
+    commitDate=true
   else
     echo "!$file not found or no changes."
   fi
+  if [ "$i" -eq $count ]; then
+    echo $sep1 >> ../../env.log.txt
+  fi
+  i=$(( i + 1 ))
 done
