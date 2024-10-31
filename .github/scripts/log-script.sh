c7d996c35e25b4ae3a9b7448ad7d9fd81ea8fa3c
mkacem <moez.kacem@keyrus.com>
2024-10-31 14:28:39 +0100

    Update log-script.sh

--- .github/scripts/log-script.sh
+++ .github/scripts/log-script.sh
@@ -10,37 +10,15 @@ for file in $propfiles; do
   if [ -f "$file" ] && [ "$diff" ]
   then  
     echo -e $sep1$date'   '$fileM$sep2$diff'\n'$sep1 >> ../../env.log.txt
-    grep -v '^#' < "$file" | 
-      while IFS='=' read -r key value
-      do
-        if [ "$key" ] 
-        then
-          testKey="${key}"
-        fi
-      done 
+    # grep -v '^#' < "$file" | 
+    #   while IFS='=' read -r key value
+    #   do
+    #     if [ "$key" ] 
+    #     then
+    #       testKey="${key}"
+    #     fi
+    #   done 
   else
     echo "!$file not found or no changes."
   fi
 done
-
-
-# file="../../app.properties" 
-# diff=$(git diff --unified=0 HEAD origin/dev -- $file  | grep -Ev '(index|@@|--git|---|\+\+\+|new)' | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' )
-# fileM=$(git diff --name-only HEAD origin/dev -- $file)
-# date=$(date '+%Y-%m-%d %H:%M:%S')
-# sep1="____________________________________________\n"
-# sep2='\n--------------------------------------------\n'
-# if [ -f "$file" ] && [ "$diff" ]
-# then  
-#   echo -e $sep1'\n'$date'   '$fileM$sep2$diff'\n'$sep1 >> changelog.txt
-#   grep -v '^#' < "$file" | 
-#     while IFS='=' read -r key value
-#     do
-#       if [ "$key" ] 
-#       then
-#         testKey="${key}"
-#       fi
-#     done 
-# else
-#   echo "!$file not found or no changes."
-# fi
